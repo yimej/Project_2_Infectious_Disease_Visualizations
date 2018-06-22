caf8f1df69ce0479c21f31eb53715da84a521c6f
Scott McAlister <33137974+scottmcalister@users.noreply.github.com>
2018-06-22 17:07:03 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -25,14 +25,6 @@ def home():
 		f"/api/v1.0/case_totals <br>")
 
 
-# This app will have to have a function that iterates through each year of the sample data,
-# figures out whether the case report was cumulative or not, if cumulative, move onto the next record with the same
-# start date until it reaches the end of the records with that start date, and adds the value count to an empty container.
-# if the record is not cumulative, it adds the value count to the container and moves onto the next row. 
-# go from years 2007-2017
-# we should return the total number of cases in each state per year, and then total for each state, and total cases in the US
-# over the last ten years.
-
 @app.route("/api.v1.0/Cases_per_year")
 def cases():
 	results=[]
