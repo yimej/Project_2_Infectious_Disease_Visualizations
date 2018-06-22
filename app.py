883b7de8ab6dcbfab92a1705d66f2f0184ceb4d8
Scott McAlister <33137974+scottmcalister@users.noreply.github.com>
2018-06-22 14:15:54 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import Session
 from aqlalchemy import create_engine, func, desc
 from flask import Flask, jsonify
 
-engine = create_engine("sqlite://XXXXXXXXX.sqlite")
+
+engine = create_engine("sqlite://data.sqlite")
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-SET UP VARIABLES IF NEEDED
+Disease_Data = Base.classes.disease_data
 
 session = Session(engine)
 
@@ -18,9 +19,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return(
-		f"Welcome to the Dengue Case Report API" </br>
-		f"Availbe routes: "</br>
-		f"/api/v1.0/case totals" </br>
+		f"Welcome to the West Nile Virus Case Report API <br>"
+		f"Availbe routes: <br>"
+		f"Cases_per_year <br>"
+		f"/api/v1.0/case_totals <br>")
 
 
 # This app will have to have a function that iterates through each year of the sample data,
@@ -30,3 +32,17 @@ def home():
 # go from years 2007-2017
 # we should return the total number of cases in each state per year, and then total for each state, and total cases in the US
 # over the last ten years.
+
+@app.route("/api.v1.0/Cases_per_year")
+def cases():
+	results=[]
+	for row in data:
+		if Disease_Data.State[i] != Disease_Data.State[i+1]:
+			results.append({Disease_Data.State[i]: CountValue[i],EndDate[i]})
+		else:
+			next row
+		return jsonify(results)
+
+
+
+
