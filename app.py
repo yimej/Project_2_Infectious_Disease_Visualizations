e15f13217736e3e231aa9bc4c059631957caa1f7
yimej <33288736+yimej@users.noreply.github.com>
2018-06-23 10:04:11 -0400

    app

--- app.py
+++ app.py
@@ -10,11 +10,7 @@ engine = create_engine("sqlite://data.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-<<<<<<< HEAD
-# SET UP VARIABLES IF NEEDED
-=======
 Disease_Data = Base.classes.disease_data
->>>>>>> caf8f1df69ce0479c21f31eb53715da84a521c6f
 
 session = Session (engine)
 
@@ -23,14 +19,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 	return(
-<<<<<<< HEAD
-		f"Welcome to the Dengue Case Report API" <br>
-		f"Available routes: "<br>
-		f"/api/v1.0/case totals" <br>
-
-
-# This app will have to have a function that iterates through each year of the sample data, figures out whether the case report was cumulative or not, if cumulative, move onto the next record with the same start date until it reaches the end of the records with that start date, and adds the value count to an empty container. if the record is not cumulative, it adds the value count to the container and moves onto the next row. go from years 2007-2017 we should return the total number of cases in each state per year, and then total for each state, and total cases in the US over the last ten years.
-=======
 		f"Welcome to the West Nile Virus Case Report API <br>"
 		f"Availbe routes: <br>"
 		f"Cases_per_year <br>"
@@ -50,4 +38,3 @@ def cases():
 
 
 
->>>>>>> caf8f1df69ce0479c21f31eb53715da84a521c6f
