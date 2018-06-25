32e2c2e38ab649bf08b50f61e6d626c34c2bd823
yimej <33288736+yimej@users.noreply.github.com>
2018-06-25 19:09:15 -0400

    spelling edits in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from aqlalchemy import create_engine, func, desc
+from sqlalchemy import create_engine, func, desc
 from flask import Flask, jsonify
 
 
