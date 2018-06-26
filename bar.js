ddaa8740ecb427f485ffdad622c1aaa8e60ce481
yimej <33288736+yimej@users.noreply.github.com>
2018-06-26 12:12:50 -0400

    bar+line data called directly from csv

--- bar.js
+++ bar.js
@@ -1,16 +1,36 @@
-var trace1 = {
-  x: ['ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'CONNECTICUT', 'DELAWARE', 'DISTRICT OF COLUMBIA', 'FLORIDA', 'GEORGIA', 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA', 'MAINE', 'MARYLAND', 'MASSACHUSETTS', 'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 'NEVADA', 'NEW HAMPSHIRE', 'NEW JERSEY', 'NEW MEXICO', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OKLAHOMA', 'OREGON', 'PENNSYLVANIA', 'RHODE ISLAND', 'SOUTH CAROLINA', 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON', 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING'],
-  y: [51.0, 0.0, 186.0, 13.0, 883.0, 535.0, 11.0, 16.0, 20.0, 12.0, 30.0, 10.0, 65.0, 273.0, 32.0, 76.0, 154.0, 7.0, 126.0, 8.0, 41.0, 14.0, 28.0, 142.0, 112.0, 65.0, 42.0, 472.0, 44.0, 4.0, 29.0, 35.0, 73.0, 142.0, 192.0, 45.0, 113.0, 14.0, 40.0, 12.0, 49.0, 357.0, 31.0, 433.0, 40.0, 5.0, 28.0, 27.0, 5.0, 27.0, 47.0],
-  type: "bar",
-  marker: {color: 'skyblue'}
+var file = 'cleaned_westnile_dataset.csv';
+
+makePlot();
+
+function makePlot() {
+  d3.csv(file, function(data){makeBar(data)});
 };
 
-var data = [trace1];
+function makeBar(dataset) {
+  var x = [];
+  var y = [];
 
-var layout = {
-  title: "Total Number of West Nile Cases per State from 2010 to 2017",
-  xaxis: {title: "State", tickangle: -45},
-  yaxis: {title: "Number of Cases"}
+  for (var i = 0; i < dataset.length - 1; i++) {
+    row = dataset[i];
+    x.push(row['State']);
+    y.push(row['TotalByState']);
+  }
+
+  var trace = {
+    x: x, 
+    y: y,
+    type: "bar",
+    marker: {color: 'skyblue'}
+  };
+
+  var data = [trace];
+  
+  var layout = {
+    title: "Total Number of West Nile Cases per State from 2010 to 2017",
+    xaxis: {title: "State", tickangle: -35},
+    yaxis: {title: "Number of Cases"},
+    font: {size: 10}
+  };
+
+  Plotly.newPlot('bar', data, layout);
 };
-
-Plotly.newPlot("bar", data, layout);
--- line.js
+++ line.js
@@ -1,16 +1,53 @@
-var trace1 = {
-    x: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
-    y: [617.0, 206.0, 1176.0, 43.0, 841.0, 698.0, 929.0, 706.0, ],
+// var trace1 = {
+//     x: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+//     y: [617.0, 206.0, 1176.0, 43.0, 841.0, 698.0, 929.0, 706.0, ],
+//     type: "line",
+//     marker: {color: 'skyblue'}
+//   };
+  
+//   var data = [trace1];
+  
+//   var layout = {
+//     title: "Total Number of West Nile Cases in U.S. by Year",
+//     xaxis: {title: "Year"},
+//     yaxis: {title: "Number of Cases"}
+//   };
+  
+//   Plotly.newPlot("line", data, layout);
+
+var file = 'cleaned_westnile_dataset.csv';
+
+makePlot();
+
+function makePlot() {
+  d3.csv(file, function(data){makeLine(data)});
+};
+
+function makeLine(dataset) {
+  var x = [];
+  var y = [];
+
+  for (var i = 0; i < dataset.length - 1; i++) {
+    row = dataset[i];
+    x.push(row['State']);
+    y.push(row['TotalByState']);
+  }
+
+  var trace = {
+    x: x, 
+    y: y,
     type: "line",
     marker: {color: 'skyblue'}
   };
-  
-  var data = [trace1];
-  
+
+  var data = [trace];
+
   var layout = {
-    title: "Total Number of West Nile Cases in U.S. by Year",
+    title: "Total Number of West Nile Cases In U.S. by Year",
     xaxis: {title: "Year"},
     yaxis: {title: "Number of Cases"}
+    // font: {size: 10}
   };
-  
-  Plotly.newPlot("line", data, layout);
+
+  Plotly.newPlot('line', data, layout);
+};
