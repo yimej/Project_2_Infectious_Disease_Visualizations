17cf44b6a0628360cdaf0b9268bc034f9f8760d2
yimej <33288736+yimej@users.noreply.github.com>
2018-06-26 23:09:38 -0400

    2010-2017 -> since 2010 (title changes)

--- bar.js
+++ bar.js
@@ -24,7 +24,7 @@ function makeBar(dataset) {
   var data = [trace];
   
   var layout = {
-    title: "Total Number of Cases per State from 2010 to 2017",
+    title: "Total Number of Cases Since 2010 by State",
     xaxis: {title: "State", tickangle: -35},
     yaxis: {title: "Number of Cases"},
     font: {size: 10}
